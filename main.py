1adeb84c9d7471746df4fda39d6c13dbd15b9810
mohammadosman <mohammadhakimosman@outlook.com>
2024-08-22 15:58:27 -0700

    form submission working

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from datetime import datetime
 
 app = Flask(__name__)
 posts = requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
 date = datetime.today().strftime('%B %d, %Y')
-print(posts)
-print(date)
+#print(posts)
+#print(date)
 
 @app.route("/")
 def get_home():
@@ -25,3 +25,13 @@ def create_post(id):
     for post in posts:
         if post['id'] == id:
             return render_template("post.html", post=post, date=date)
+        
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        return render_template("form-entry.html", name=name, email=email, phone=phone, 
+                               message=message)
