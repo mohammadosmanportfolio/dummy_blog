8ed52bf89af14d01002ae6300db1ac4609da46ce
mohammadosman <mohammadhakimosman@outlook.com>
2024-08-22 16:49:54 -0700

    played around with 'GET' and 'POST' methods for the '/contact' route.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 from datetime import datetime
 
@@ -16,9 +16,17 @@ def get_home():
 def get_about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def get_contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        return render_template('form-entry.html', name=name, email=email, phone=phone,
+                               message=message)
+    elif request.method == "GET":
+        return render_template("contact.html")
 
 @app.route("/<int:id>")
 def create_post(id):
@@ -26,12 +34,11 @@ def create_post(id):
         if post['id'] == id:
             return render_template("post.html", post=post, date=date)
         
-@app.route("/form-entry", methods=["POST"])
-def receive_data():
-    if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        phone = request.form["phone"]
-        message = request.form["message"]
-        return render_template("form-entry.html", name=name, email=email, phone=phone, 
-                               message=message)
+# @app.route("/form-entry", methods=["GET", "POST"])
+# def receive_data():
+#     if request.method == "POST":
+#         name = request.form["name"]
+#         email = request.form["email"]
+#         phone = request.form["phone"]
+#         message = request.form["message"]
+#         return redirect(url_for('contact')) 
